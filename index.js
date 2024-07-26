11fcd2c242de989166d47c362b0c2c838ec5cd65
Marsden Mathews Odhiambo <marsdenlwero@gmail.com>
2024-07-26 10:49:38 +0300

    Test completed

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor((Math.random() * 10) + 1);
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+
+let num5 = 20;
+let max = Math.max(num2, num3, num4, num5);
